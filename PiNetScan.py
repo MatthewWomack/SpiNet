47cb55d2026924f095c4020f3d11291de56ad162
MatthewWomack <mowomack1@catamount.wcu.edu>
2025-12-28 17:45:58 -0500

    maybe faster, idk

--- PiNetScan.py
+++ PiNetScan.py
@@ -39,7 +39,7 @@ def check_vulnerabilities(host):
     hostname = host['hostname'] if host['hostname'] else "Unknown"
     
     nm = nmap.PortScanner()
-    nm.scan(host_ip, arguments='-sV --script vuln -p 1-79,81-8080')
+    nm.scan(host_ip, arguments='-sV --script vuln')
     vulns = []
     
     for proto in nm[host_ip].all_protocols():
